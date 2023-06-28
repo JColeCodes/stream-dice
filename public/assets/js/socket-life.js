fb7f80774986ca2ce9a9b53981a728ec4a460f41
Jennifer Cole <capauldi@gmail.com>
2023-06-27 23:49:42 -0700

    updated card animation in socket + added some images

--- public/assets/js/socket-life.js
+++ public/assets/js/socket-life.js
@@ -6,20 +6,23 @@ function randomize(min, max) {
 
 function displayCard(card) {
     const cardImg = document.querySelector('.card-img');
-    cardImg.innerHTML = `<img src="/assets/images/${cards[card].image}.png"
-        alt="${cards[card].title}" />
-        <p>${cards[card].title}</p>`;
+    // cardImg.innerHTML = `<img src="/assets/images/${cards[card].image}.png"
+    //     alt="${cards[card].title}" />
+    //     <p>${cards[card].title}</p>`;
 }
-
 
 if (document.querySelector('#pick-card-type')) {
     document.querySelector('#pick-card-type').addEventListener('change', event => {
-        console.log(event.target.value);
         if (event.target.value === 'select-type') {
             document.querySelector('#pick-card').classList.add('hidden');
         } else {
             document.querySelector('#pick-card').classList.remove('hidden');
         }
+
+        socket.emit('life', {
+            current: 'card-type',
+            value: event.target.value
+        });
     });
 
     document.querySelector('#pick-card').addEventListener('click', (event) => {
@@ -27,7 +30,7 @@ if (document.querySelector('#pick-card-type')) {
         
         // Emit the following information
         socket.emit('life', {
-            current: 'card',
+            current: 'card-face',
             card: randomize(0, 6)
         });
     });
@@ -41,7 +44,7 @@ socket.on('life', (data) => {
         diceRoll(times, data.dice);
 
     }
-    else if (data.current === 'card') {
+    else if (data.current === 'card-face') {
         const card = document.querySelector('.card');
         if (card.classList.contains('flip')) {
             card.classList.toggle('flip');
@@ -58,6 +61,44 @@ socket.on('life', (data) => {
         }
         setTimeout(function() {
             card.classList.add('flip');
+        }, 60000);
+    } else if (data.current === 'card-type') {
+        const card = document.querySelector('.card');
+        const cardBack = document.querySelector('.card-back');
+
+        if (data.value === 'select-type') {
+            card.classList.add('card-hidden');
+            card.classList.remove('card-show');
+        } else {
+            if (card.classList.contains('card-show')) {
+                card.classList.remove('card-show');
+                card.classList.add('card-hidden');
+            }
+            setTimeout(() => {
+                if (card.classList.contains('card-hidden')) {
+                    card.classList.remove('card-hidden');
+                    card.classList.add('card-show');
+                }
+                cardBack.classList.remove('career');
+                cardBack.classList.remove('house');
+                cardBack.classList.remove('action');
+
+                if (data.value === 'career') {
+                    cardBack.classList.add('career');
+                }
+                else if (data.value === 'house') {
+                    cardBack.classList.add('house');
+                }
+                else if (data.value === 'action') {
+                    cardBack.classList.add('action');
+                }
+            }, 200);
+        }
+
+        // Hides after 1 minute
+        setTimeout(() => {
+            card.classList.remove('card-show');
+            card.classList.add('card-hidden');
         }, 60000);
     }
 });
