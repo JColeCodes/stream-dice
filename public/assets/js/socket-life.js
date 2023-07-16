54ad9fcf4e65dd85ca7855131d1eeb8d8a48d486
Jennifer Cole <capauldi@gmail.com>
2023-07-15 19:37:50 -0700

    card selection now saves a list of selected career and house cards + new window opens for game instructions

--- public/assets/js/socket-life.js
+++ public/assets/js/socket-life.js
@@ -1,5 +1,9 @@
 const socket = io(); // SOCKET.IO
 
+const cardStack = document.querySelector('.card-stack');
+const leftCard = cardStack.querySelector('.left');
+const rightCard = cardStack.querySelector('.right');
+
 function randomize(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
@@ -48,7 +52,6 @@ function displayCard(card, card_type, side) {
 }
 
 function selectCard(side) {
-    const cardStack = document.querySelector('.card-stack');
     let other = '';
     if (side === 'left') {
         other = 'right';
@@ -69,6 +72,80 @@ function removeInit() {
     });
 }
 removeInit();
+
+
+function addMyCards(card) {
+    const cardList = document.querySelector('.personal-view .card-list');
+    const noCardsMsg = document.querySelector('.no-cards-msg');
+
+    const cardNum = parseInt(card.dataset.num);
+    const cardType = cardStack.dataset.type;
+
+    if (cardType === 'career') {
+        if (document.querySelector('.personal-view .list-career')) {
+            document.querySelector('.personal-view .list-career').remove();
+        }
+        const listCareer = document.createElement('div');
+        listCareer.classList.add('list-career');
+        listCareer.innerHTML = `<h1>${careerCards[cardNum].title}</h1>
+        <img src="assets/images/LIFE/${cardType}/${careerCards[cardNum].image}.png"
+            alt="${careerCards[cardNum].title}" class="career-img" />
+        <div class="salary-table">
+            <div class="salary">
+                <h2>Salary</h2>
+                <ul>
+                    <li><img src="assets/images/LIFE/icons/money.svg" alt="Base Salary" /> ${careerCards[cardNum].nocollege.salary}k</li>
+                    <li><img src="assets/images/LIFE/icons/college.svg" alt="College Salary" /> ${careerCards[cardNum].college.salary}k</li>
+                    <li><img src="assets/images/LIFE/icons/collegex2.svg" alt="Extended College Salary" /> ${careerCards[cardNum].university.salary}k</li>
+                </ul>
+            </div>
+            <div class="taxes">
+                <h2>Taxes</h2>
+                <ul>
+                    <li><img src="assets/images/LIFE/icons/money.svg" alt="Base Tax" /> ${careerCards[cardNum].nocollege.taxes}k</li>
+                    <li><img src="assets/images/LIFE/icons/college.svg" alt="College Tax" /> ${careerCards[cardNum].college.taxes}k</li>
+                    <li><img src="assets/images/LIFE/icons/collegex2.svg" alt="Extended College Tax" /> ${careerCards[cardNum].university.taxes}k</li>
+                </ul>
+            </div>
+        </div>`;
+        cardList.prepend(listCareer);
+
+    } else if (cardType === 'house') {
+        const listHouse = document.createElement('div');
+        listHouse.classList.add('list-house');
+        listHouse.innerHTML = `<h1>${houseCards[cardNum].title}</h1>
+        <img src="assets/images/LIFE/${cardType}/${houseCards[cardNum].image}.png"
+            alt="${houseCards[cardNum].title}" class="house-img" />
+        <p class='house-price'>$${houseCards[cardNum].price},000</p>
+        <hr />
+        <div class='sell-house'>
+            <span>SELL!</span>
+            <div class='sell-prices'>
+                <p class="sell-loss"><img src="assets/images/LIFE/icons/spintowin-red.svg" alt="Spin Red" /> $${houseCards[cardNum].sell.red},000</p>
+                <p class="sell-gain"><img src="assets/images/LIFE/icons/spintowin-black.svg" alt="Spin Black" /> $${houseCards[cardNum].sell.black},000</p>
+            </div>
+        </div>`;
+
+        const closeHouse = document.createElement('div');
+        closeHouse.classList.add('close-house');
+        closeHouse.innerHTML = `<i class="fa-solid fa-circle-xmark"></i>`;
+        closeHouse.addEventListener('click', () => {
+            listHouse.remove();
+            if (cardList.innerHTML === '') {
+                noCardsMsg.classList.remove('hidden');
+            }
+        });
+        listHouse.appendChild(closeHouse);
+
+        cardList.appendChild(listHouse);
+    }
+
+    if (cardList.innerHTML !== '') {
+        noCardsMsg.classList.add('hidden');
+    } else {
+        noCardsMsg.classList.remove('hidden');
+    }
+}
 
 if (document.querySelector('#pick-card-type')) {
     document.querySelector('#spin-wheel').addEventListener('click', e=> {
@@ -117,49 +194,54 @@ if (document.querySelector('#pick-card-type')) {
         })
     });
 
-    const cardStack = document.querySelector('.card-stack');
     const cardBottom = document.querySelector('.card.bottom');
     cardBottom.addEventListener('click', e => {
         e.preventDefault();
 
-        let cardInfoA = 0;
-        let cardInfoB = 0;
+        let cardInfoL = 0;
+        let cardInfoR = 0;
         const typeID = cardStack.dataset.type;
 
-        if (cardStack.querySelector('.left').classList.contains('flip')) {
+        if (leftCard.classList.contains('flip')) {
             if (typeID === 'career') {
-                cardInfoA = randomize(0, 20);
-                cardInfoB = randomize(0, 20);
+                cardInfoL = randomize(0, 20);
+                cardInfoR = randomize(0, 20);
 
-                while (cardInfoA === cardInfoB) {
-                    cardInfoB = randomize(0, 20);
+                while (cardInfoL === cardInfoR) {
+                    cardInfoR = randomize(0, 20);
                 }
             }
             else if (typeID === 'house') {
-                cardInfoA = randomize(0, 10);
-                cardInfoB = randomize(0, 10);
+                cardInfoL = randomize(0, 10);
+                cardInfoR = randomize(0, 10);
 
-                while (cardInfoA === cardInfoB) {
-                    cardInfoB = randomize(0, 10);
+                while (cardInfoL === cardInfoR) {
+                    cardInfoR = randomize(0, 10);
                 }
             }
             else if (typeID === 'action') {
-                cardInfoA = randomize(0, 1);
-                cardInfoB = randomize(0, 1);
+                cardInfoL = randomize(0, 1);
+                cardInfoR = randomize(0, 1);
             }
         }
+
+        leftCard.dataset.num = cardInfoL;
+        rightCard.dataset.num = cardInfoR;
 
         socket.emit('life', {
             current: 'card-flip',
             type: typeID,
-            cardA: cardInfoA,
-            cardB: cardInfoB,
+            cardL: cardInfoL,
+            cardR: cardInfoR,
         });
     });
 
     const cardLeft = document.querySelector('.card.left');
     cardLeft.addEventListener('click', e => {
         e.preventDefault();
+
+        addMyCards(cardLeft);
+
         socket.emit('life', {
             current: 'card-left-select'
         });
@@ -168,9 +250,49 @@ if (document.querySelector('#pick-card-type')) {
     const cardRight = document.querySelector('.card.right');
     cardRight.addEventListener('click', e => {
         e.preventDefault();
+
+        addMyCards(cardRight);
+
         socket.emit('life', {
             current: 'card-right-select'
         });
+    });
+
+    document.querySelector('#how-to-play').addEventListener('click', () => {
+        const infoPanel = window.open('', 'infoPanel', 'width=550,height=722');
+        infoPanel.document.write(`
+        <html lang="en">
+            <head>
+                <title>How To Play</title>
+                <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Luckiest+Guy&family=Milonga&family=Akshar:wght@300;500;700&family=Lato:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+                <link rel='stylesheet' href='/assets/css/style.css' />
+                <link rel='stylesheet' href='/assets/css/life.css' />
+                <link rel='icon' href='/assets/images/fez.png' />
+            </head>
+            <body>
+                <div class="info-panel">
+                    <h3>How to play</h3>
+                    <div class="info-panel-content">
+                        <i>Any spin you make, any card you pick, EVERY player can see it.</i>
+                        <h4>The Spinner</h4>
+                        <ul>
+                            <li>The default spinner is the NUMBERS wheel, but you can toggle on the FATE wheel (via the purple button) for when you land on a fate circle.</li>
+                            <li>To spin the wheel, you press the "Spin the Wheel" button, which will pop the result in front of the spinner when the spin finishes. You can close the result by pressing the pink "Close Spin Result" button. (It will automatically hide after one minute.)</li>
+                        </ul>
+                        <h4>The Cards</h4>
+                        <ul>
+                            <li>To select a card, press a button to determine which type of card you're selecting. "Career", "House", or "Action". Once selected, the card stack will slide in from the right.</li>
+                            <li>Click on the card stack once to draw 2 cards, which will flip over and show side by side.</li>
+                            <li>You can select your card by clicking on the one you pick. Doing that will put your selected card in the front view.</li>
+                            <li>You can click on your selected card once more to put them back in the stack for the next person.</li>
+                            <li>You can also hide the card stack by pressing the pink "Reset Cards" button, which will put all cards back into the stack and/or move the stack off screen. (It will automatically do this 2 minutes after the initial card stack call.)</li>
+                            <li>All cards listed under "My Cards" are visible only to you in this session. If you refresh, your cards will disappear.</li>
+                            <li>In "My Cards", selecting a new career will replace the career card (only 1 career at a time), but all new houses will be added (you can own multiple houses). To remove a specific house card, hover over the card and a red "x" will appear. Click the "x" to get rid of the card.</li>
+                        </ul>
+                    </div>
+                </div>
+            </body>
+        </html>`);
     });
 
 }
@@ -250,7 +372,7 @@ socket.on('life', (data) => {
         }, 5500);
         setTimeout(() => {
             resultEl.classList.add('hidden');
-        }, 120000);
+        }, 60000);
 
     } else if (data.current === 'spinner-toggle') {
         const spinner = document.querySelector('.spin');
@@ -286,8 +408,6 @@ socket.on('life', (data) => {
         }
 
     } else if (data.current === 'card-type') {
-        const cardStack = document.querySelector('.card-stack');
-
         cardStack.dataset.type = data.type;
 
         document.querySelectorAll('.card').forEach(card => {
@@ -340,17 +460,13 @@ socket.on('life', (data) => {
                 }, 200);
             }
 
-            // Hides after 1.2 minutes
+            // Hides after 2 minutes
             setTimeout(() => {
                 card.classList.remove('card-show');
                 card.classList.add('card-hidden');
             }, 120000);
         });
     } else if (data.current === 'card-flip') {
-        const cardStack = document.querySelector('.card-stack');
-        const leftCard = cardStack.querySelector('.left');
-        const rightCard = cardStack.querySelector('.right');
-
         let flipTimeout = 0;
         if (leftCard.classList.contains('selected') || leftCard.classList.contains('unselected') || rightCard.classList.contains('selected') || rightCard.classList.contains('unselected') ) {
             flipTimeout = 700;
@@ -366,7 +482,7 @@ socket.on('life', (data) => {
             if (leftCard.classList.contains('flip')) {
                 leftCard.classList.remove('flip');
                 leftCard.classList.add('move-left');
-                displayCard(data.cardA, data.type, '.left');
+                displayCard(data.cardL, data.type, '.left');
             } else {
                 leftCard.classList.add('flip');
                 leftCard.classList.remove('move-left');
@@ -375,7 +491,7 @@ socket.on('life', (data) => {
             if (rightCard.classList.contains('flip')) {
                 rightCard.classList.remove('flip');
                 rightCard.classList.add('move-right');
-                displayCard(data.cardB, data.type, '.right');
+                displayCard(data.cardR, data.type, '.right');
             } else {
                 rightCard.classList.add('flip');
                 rightCard.classList.remove('move-right');
